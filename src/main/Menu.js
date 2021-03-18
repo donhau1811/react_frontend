55f2840ee0aa5a92900e9926bfe9fa0db89a5ece
Hau Tran <trandonhau1811@gmail.com>
2021-03-18 21:23:37 +0200

    Change to UI

--- src/main/Menu.js
+++ src/main/Menu.js
@@ -3,7 +3,7 @@ import { Link, withRouter } from "react-router-dom";
 import { signout, isAuthenticated } from "../auth";
 
 const isActive = (history, path) => {
-  if (history.location.pathname === path) return { color: "#ff9900" };
+  if (history.location.pathname === path) return { color: "#ff0000" };
   else return { color: "#ffffff" };
 };
 
